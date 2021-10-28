8bdc317b983545cbd69ed744466484907626c0ac
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-10-28 11:07:45 +0100

    Tennis kata with parameterized unit tests.

---
+++ TennisKata.Tests/TennisKataTest.cs
@@ -0,0 +1,121 @@
+using NUnit.Framework;
+
+namespace TennisKata.Tests
+{
+    public class Tests
+    {
+        private TennisGame game;
+        
+        [SetUp]
+        public void Setup()
+        {
+            game = new TennisGame();
+        }
+
+        [Test]
+        public void WhenTwoPlayersStartATennisGameThenBothScoresAreZero()
+        {
+            string expectedResult = "love-all";
+
+            game = new TennisGame();
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void WhenTwoPlayersHaveBothScoredOnceThenReturnFifteenAll()
+        {
+            string expectedResult = "fifteen-all";
+
+            game = new TennisGame();
+            game.playerOneScored();
+            game.playerTwoScored();
+
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void WhenTwoPlayersHaveBothScoredTwiceThenReturnThirtyAll()
+        {
+            string expectedResult = "thirty-all";
+
+            game = new TennisGame();
+            game.playerOneScored();
+            game.playerOneScored();
+            game.playerTwoScored();
+            game.playerTwoScored();
+
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void WhenTwoPlayersHaveBothScoredThreeTimesThenReturnFourtyAll()
+        {
+            string expectedResult = "forty-all";
+
+            game = new TennisGame();
+            game.playerOneScored();
+            game.playerOneScored();
+            game.playerOneScored();
+
+            game.playerTwoScored();
+            game.playerTwoScored();
+            game.playerTwoScored();
+
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void WhenTwoPlayersHaveBothScoredFourTimesThenReturnDeuce()
+        {
+            string expectedResult = "deuce";
+
+            game = new TennisGame();
+            game.playerOneScored();
+            game.playerOneScored();
+            game.playerOneScored();
+            game.playerOneScored();
+            game.playerTwoScored();
+            game.playerTwoScored();
+            game.playerTwoScored();
+            game.playerTwoScored();
+            
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(1, 0, "fifteen-love")]
+        [TestCase(0, 1, "love-fifteen")]
+        // TODO Iterate to ALL permutations
+        [TestCase(5, 0, "player one wins")]
+        [TestCase(0, 5, "player two wins")]
+        [TestCase(4, 3, "advantage player one")]
+        [TestCase(3, 4, "advantage player two")]
+        public void ParameterizedTestingOfPlayerScoresToResult(int playerOneScore, int playerTwoScore,
+            string expectedResult)
+        {
+            game = new TennisGame();
+            for (int i = 0; i < playerOneScore; i++)
+            {
+                game.playerOneScored();
+            }
+
+            for (int i = 0; i < playerTwoScore; i++)
+            {
+                game.playerTwoScored();
+            }
+
+            string result = game.getScore();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ TennisKata/TennisGame.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TennisKata
+{
+    public class TennisGame
+    {
+        private int playerOneScore;
+        private int playerTwoScore;
+
+        public TennisGame()
+        {
+            playerOneScore = 0;
+            playerTwoScore = 0;
+        }
+
+        public string getScore()
+        {
+            if (isEqualScore())
+            {
+                if (playerOneScore > 3)
+                {
+                    return "deuce";
+                }
+                return getScoreName(playerOneScore) + "-all";
+            }
+            else
+            {
+                string score;
+                if (playerOneScore >= 4 || playerTwoScore >= 4)
+                {
+                    int minusResult = playerOneScore - playerTwoScore;
+                    if (minusResult == 1)
+                    {
+                        score = "advantage player one";
+                    } else if (minusResult == -1)
+                    {
+                        score = "advantage player two";
+                    } else if (minusResult >= 2)
+                    {
+                        score = "player one wins";
+                    }
+                    else
+                    {
+                        score = "player two wins";
+                    }
+                    return score;
+                }
+                return getScoreName(playerOneScore) + "-" + getScoreName(playerTwoScore);
+            }
+        }
+
+        public string getScoreName(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "love";
+                case 1:
+                    return "fifteen";
+                case 2:
+                    return "thirty";
+                case 3:
+                    return "forty";
+                default:
+                    return "";
+            }
+        }
+
+        private Boolean isEqualScore()
+        {
+            return playerOneScore == playerTwoScore;
+        }
+
+        public void playerOneScored()
+        {
+            playerOneScore++;
+        }
+        public void playerTwoScored()
+        {
+            playerTwoScore++;
+        }
+    }
+}
